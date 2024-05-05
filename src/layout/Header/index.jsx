4131754bc8683b2a9bbd65ca40f5779c80b538c6
Tal Ben Adon <talbenadon2@gmail.com>
2024-05-05 08:14:08 +0300

    commit

--- src/layout/Header/index.jsx
+++ src/layout/Header/index.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import styles from './styles.module.scss'
 import HeaderNavbar from '../../componnents/HeaderNavbar'
 import { LiaAirbnb } from "react-icons/lia";
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 export default function Header() {
+    const location = useLocation()
+    console.log(location.pathname);
     return (
         <>
             <div className={styles.screenWidth}>
-                <header className={styles.header}>
+                <header className={location.pathname === '/' ? styles.mainHeader : styles.header}>
                     <div>
                         <div className={styles.logoDiv}> <LiaAirbnb className={styles.icon} />airbnb</div>
                     </div>
